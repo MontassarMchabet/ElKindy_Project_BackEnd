e6a965330fdbffb8d6c609be38a066e9a627a591
lorinzoo <mchabetmontasar123@gmail.com>
2024-02-24 22:30:09 +0100

    v0.2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const authRouter = require('./Routes/Auth');
 const planningRouter = require('./routes/planningRoutes');
+const CourseRouter = require('./Routes/CourseRoutes');
+const RoomRouter = require('./Routes/RoomRoutes');
 const connectdb = require('./db');
 var cors = require('cors')
 require('dotenv').config();
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/plannings', planningRouter);
+app.use('/api/Course', CourseRouter);
+app.use('/api/Room', RoomRouter);
 
 connectdb();
 app.listen(process.env.port, function () {
