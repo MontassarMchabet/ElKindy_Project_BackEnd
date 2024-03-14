9070509a479e9cc129d38145ca0ca4b5239c9ad0
Ons Khiari <onskhiari2001@gmail.com>
2024-03-14 17:18:58 +0100

    updates

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -274,7 +274,7 @@ const registerClient = async (req, res) => {
             fatherOccupation: fatherOccupation ? fatherOccupation : "",
             motherOccupation: motherOccupation ? motherOccupation : "",
             isSubscribed: false,
-            level: level ? level : "",
+            level: level ? level : "Initiation",
         });
         await newClient.save();
 
