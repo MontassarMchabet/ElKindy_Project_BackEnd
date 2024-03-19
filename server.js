54f5c5b70b4c5e9aa2173ee7857e1bc75960348f
Ons Khiari <onskhiari2001@gmail.com>
2024-03-19 10:11:07 +0100

    updates

--- server.js
+++ server.js
@@ -4,56 +4,39 @@ const exam = require('./Routes/ExamR');
 const answer = require('./Routes/AnswerR');
 const note = require('./Routes/NoteR');
 const UploadImage = require('./Controllers/UploadImage');
-
 const planningRouter = require('./routes/planningRoutes');
 const CourseRouter = require('./Routes/CourseRoutes');
 const RoomRouter = require('./Routes/RoomRoutes');
-var cors = require('cors')
-
 const eventRouter = require('./Routes/Event');
 const commentRouter = require('./Routes/Comment');
 const ticketRouter = require('./Routes/Ticket');
-
-
 const product = require('./Routes/Product');
 const connectdb = require('./Config/db');
-
 const cookieParser = require('cookie-parser');
-
 
 require('dotenv').config();
 var cors = require('cors')
 var app = express()
+
+
 app.use(cookieParser());
 app.use(express.json());
-
 app.use(cors());
-
-
 app.use('/api/auth', authRouter);
 app.use('/api/exam', exam);
 app.use('/api/answer', answer);
 app.use('/api/note', note);
 app.use('/api/image', UploadImage);
-
-
-
 app.use('/api/auth', authRouter);
 app.use('/api/plannings', planningRouter);
 app.use('/api/Course', CourseRouter);
 app.use('/api/Room', RoomRouter);
-
 app.use('/event', eventRouter);
 app.use('/comment', commentRouter);
 app.use('/tickets', ticketRouter);
-
 app.use('/api/product', product);
-
-
-
 app.use('/api/auth', authRouter);
 app.use('/api/image', UploadImage);
-
 
 
 connectdb();
